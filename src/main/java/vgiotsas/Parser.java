ef835599a9d0b4fbe5ae914ccf6558bf1c894b42
vgiotsas <vgiotsas@cs.ucl.ac.uk>
2018-04-25 05:37:49 +0300

    Interface for parser

---
+++ src/main/java/vgiotsas/Parser.java
@@ -0,0 +1,33 @@
+package vgiotsas;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public interface Parser {
+    void startParser();
+
+    void writeResults(int startTs, int endTs, HashMap<String, Result.TimeLine> communitiesTimeline);
+
+    ArrayList<String> removePrepending(String[] path);
+
+    HashMap<String, Integer> readRelationships(String fileIn);
+
+    String constructOptionalArgs(String collectors, String communities, String peers, String prefixes);
+
+    String[] mapCommunityToLink(ArrayList<String> path, String communityTop16);
+
+    Result getAnnotatedPaths(String optionalArgs, int init_start, int init_end, List<String> targetFacilities, int requestedOverlap);
+
+    HashMap<String, HashMap<String, Route>> filterUnstablePaths(
+            String optionalArgs, HashMap<String,
+            HashMap<String, Route>> initialRoutes,
+            int start_ts,
+            int end_ts);
+
+    HashMap<String, HashMap<String, Route>> monitorAnnotatedPaths(
+            String optionalArgs, HashMap<String,
+            HashMap<String, Route>> annotatedRoutes,
+            int start_ts,
+            int end_ts);
+}
